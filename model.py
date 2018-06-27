b0f10662ec70d54fc2500e1d723d65358119607f
Eren <egolge@mozilla.com>
2018-06-27 17:34:46 +0200

    Add step forward with buffering for faster ingerence

--- model.py
+++ model.py
@@ -4,27 +4,33 @@ from torch import nn
 
 
 class FFTNetQueue(object):
-    def __init__(self, size, num_channels):
+    def __init__(self, batch_size, size, num_channels, cuda=True):
         super(FFTNetQueue, self).__init__()
         self.size = size
+        self.batch_size = batch_size
         self.num_channels = num_channels
+        self.cuda = cuda
         self.queue1 = []
         self.queue2 = []
         self.reset()
-        
+
     def reset(self):
-        self.queue1 = torch.zeros([self.size, self.num_channels])
-        self.queue2 = torch.zeros([self.size, self.num_channels])
-        
-    def enqueue(self, item1, item2):
-        self.queue1[:-1] = self.queue1[1:]
-        self.queue1[-1] = item1
-        self.queue2[:-1] = self.queue2[1:]
-        self.queue2[-1] = item2
-        
-    
+        self.queue1 = torch.zeros([self.batch_size, self.num_channels, self.size])
+        self.queue2 = torch.zeros([self.batch_size, self.num_channels, self.size])
+        if self.cuda:
+            self.queue1 = self.queue1.cuda()
+            self.queue2 = self.queue2.cuda()
+
+    def enqueue(self, x):
+        self.queue2[:, :, :-1] = self.queue2[:, :, 1:]
+        self.queue2[:, :, -1] = self.queue1[:, :, 0]
+        self.queue1[:, :, :-1] = self.queue1[:, :, 1:]
+        self.queue1[:, :, -1] = x.view(x.shape[0], x.shape[1])
+
+
 class FFTNet(nn.Module):
-    def __init__(self, in_channels, out_channels, hid_channels, layer_id, cond_channels=None, std_f=1.0):
+    def __init__(self, in_channels, out_channels, hid_channels, layer_id,
+                 cond_channels=None, std_f=1.0):
         super(FFTNet, self).__init__()
         self.layer_id = layer_id
         self.receptive_field = 2**layer_id
@@ -38,13 +44,12 @@ class FFTNet(nn.Module):
         if cond_channels is not None:
             self.convc1 = nn.Conv1d(cond_channels, hid_channels, 1)
             self.convc2 = nn.Conv1d(cond_channels, hid_channels, 1)
-            self.cond_queue = FFTNetQueue(self.K, hid_channels)
         self.conv2 = nn.Conv1d(hid_channels, out_channels, 1)
         self.relu = nn.ReLU()
         self.init_weights(std_f)
-        self.input_queue = FFTNetQueue(self.K, hid_channels)
-        
-    
+        self.buffer = None
+        self.cond_buffer = None
+
     def init_weights(self, std_f):
         std = np.sqrt(std_f / self.in_channels)
         self.conv1_1.weight.data.normal_(mean=0, std=std)
@@ -56,8 +61,8 @@ class FFTNet(nn.Module):
             self.convc1.bias.data.zero_()
             self.convc2.weight.data.normal_(mean=0, std=std)
             self.convc2.bias.data.zero_()
-        
-        
+
+
     def forward(self, x, cx=None):
         """
         Shapes:
@@ -65,7 +70,7 @@ class FFTNet(nn.Module):
             cx: batch x cond_channels x time
             out: batch x out_chennels x time - receptive_field/2
         """
-        T = x.shape[2] 
+        T = x.shape[2]
         x1 = x[:, :, :-self.K]
         x2 = x[:, :, self.K:]
         z1 = self.conv1_1(x1)
@@ -82,20 +87,41 @@ class FFTNet(nn.Module):
         out = self.conv2(out)
         out = self.relu(out)
         return out
-    
-    def forward_step(self):
-        
-    
-    
+
+    def forward_step(self, x, cx=None):
+        T = x.shape[2]
+        B = x.shape[0]
+        if self.buffer is None:
+            self.buffer = FFTNetQueue(B, self.K, self.in_channels, x.is_cuda)
+        if self.cond_channels is not None and self.cond_buffer is None:
+            self.cond_buffer = FFTNetQueue(B, self.K, self.cond_channels, x.is_cuda)
+        self.buffer.enqueue(x)
+        if self.cond_channels is not None:
+            self.cond_buffer.enqueue(cx)
+        x_input = self.buffer.queue1[:, :, 0].view([1, self.in_channels, -1])
+        x_input2 = self.buffer.queue2[:, :, 0].view([1, self.in_channels, -1])
+        z1 = self.conv1_1(x_input)
+        z2 = self.conv1_2(x_input2)
+        z = z1 + z2
+        if cx is not None:
+            cx1 = self.cond_buffer.queue1[:, :, 0].view(1, self.cond_channels, -1)
+            cx2 = self.cond_buffer.queue2[:, :,  0].view(1, self.cond_channels, -1)
+            cz1 = self.convc1(cx1)
+            cz2 = self.convc2(cx2)
+            z = z + cz1 + cz2
+        return z
+
+
 class FFTNetModel(nn.Module):
-    def __init__(self, hid_channels=256, out_channels=256, n_layers=11, cond_channels=None):
+    def __init__(self, hid_channels=256, out_channels=256, n_layers=11,
+                 cond_channels=None):
         super(FFTNetModel, self).__init__()
         self.cond_channels = cond_channels
         self.hid_channels = hid_channels
         self.out_channels = out_channels
         self.n_layers = n_layers
         self.receptive_field = 2 ** n_layers
-        
+
         self.layers = []
         for idx in range(self.n_layers):
             layer_id = n_layers - idx
@@ -106,12 +132,12 @@ class FFTNetModel(nn.Module):
             self.layers.append(layer)
         self.layers = nn.ModuleList(self.layers)
         self.fc = nn.Linear(hid_channels, out_channels)
-        
+
     def forward(self, x, cx=None):
         """
         Shapes:
             x: batch x 1 x time
-            cx: batch x dim x time 
+            cx: batch x dim x time
         """
         # FFTNet modules
         out = x
@@ -123,8 +149,20 @@ class FFTNetModel(nn.Module):
         out = out.transpose(1, 2)
         out = self.fc(out)
         return out
-    
-    
+
+    def forward_step(self, x, cx=None):
+        # FFTNet modules
+        out = x
+        for idx, layer in enumerate(self.layers):
+            if idx == 0 and cx is not None:
+                out = layer.forward_step(out, cx)
+            else:
+                out = layer.forward_step(out)
+        out = out.transpose(1, 2)
+        out = self.fc(out)
+        return out
+
+
 def sequence_mask(sequence_length):
     max_len = sequence_length.data.max()
     batch_size = sequence_length.size(0)
