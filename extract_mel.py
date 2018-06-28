91cd4b7db517be50e9ebabd1c00f53d547ce57ae
Eren <egolge@mozilla.com>
2018-06-28 17:11:23 +0200

    updates for python 2.7

--- extract_mel.py
+++ extract_mel.py
@@ -3,11 +3,10 @@ import sys
 import time
 import glob
 import argparse
-import soundfile as sf
+# import soundfile as sf
 import librosa
 import numpy as np
 import tqdm
-import pyworld as pw
 from audio import AudioProcessor
 from generic_utils import load_config
 
@@ -31,10 +30,11 @@ OUT_PATH = args.out_path
 CONFIG = load_config(args.config)
 ap = AudioProcessor(CONFIG.sample_rate, CONFIG.num_mels, CONFIG.num_freq, CONFIG.min_level_db,
                     CONFIG.frame_shift_ms, CONFIG.frame_length_ms, CONFIG.preemphasis,
-                    CONFIG.ref_level_db)         
-        
+                    CONFIG.ref_level_db)
+
 def extract_mel(file_path):
-    x, fs = sf.read(file_path)
+    # x, fs = sf.read(file_path)
+    x, fs = librosa.load(file_path, CONFIG.sample_rate)
     mel = ap.melspectrogram(x.astype('float32'))
     file_name = os.path.basename(file_path).replace(".wav","")
     mel_file = file_name + ".mel"
@@ -54,10 +54,10 @@ if __name__ == "__main__":
 
     with Pool(20) as p:
         r = list(tqdm.tqdm(p.imap(extract_mel, file_names), total=len(file_names)))
-    
+
     file_path = os.path.join(OUT_PATH, "meta_fftnet.csv")
     file = open(file_path, "w")
     for line in r:
-        line = ", ".join(line) 
+        line = ", ".join(line)
         file.write(line+'\n')
     file.close()
